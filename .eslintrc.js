ce059d709bbe32f7c967f233bbddac6e408abe5d
Marcos Borges <marcos.abb52@gmail.com>
2021-02-18 19:02:47 -0300

    chore(linter): add initial linter configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,35 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'standard-jsx',
+    'standard-react',
+    'plugin:react-hooks/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'no-use-before-define': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
